0bc55c41f6e2b38b741df754a0e6a55a8c6c42aa
Alexey <stalpetc@gmail.com>
2023-10-23 12:34:52 +0300

    add edit buttons

--- script.js
+++ script.js
@@ -40,14 +40,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (contact.group === group) {
                     const contactDiv = document.createElement('div')
                     contactDiv.className = 'contactDiv'
-                    contactDiv.textContent = `${contact.name}(${contact.number})`
-    
-                    const deleteButton = document.createElement('button')
-                    // deleteButton.className = 'btn btn-danger'
-                    deleteButton.innerHTML = '<i class="bi bi-trash"></i>'
+                    contactDiv.innerHTML = `<div>
+                                                <span>${contact.name}</span>                                                
+                                            </div>`   
+
+                    const groupButtons = document.createElement('div')
+                    groupButtons.className = 'groupButtons'
+                    const deleteButton = document.createElement('div')
+                    deleteButton.innerHTML = `<span>${contact.number}</span>
+                                                <button type="button" class="btn btn-outline-secondary">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                                    </svg>
+                                                </button>`
+
+                    const editButton = document.createElement('div')
+                    editButton.innerHTML = `<button type="button" class="btn btn-outline-secondary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
+                                                </svg>
+                                            </button>`
+
                     deleteButton.addEventListener('click', () => deleteContact(contact))
-    
-                    contactDiv.appendChild(deleteButton)
+                    editButton.addEventListener('click', () => editContact(contact))
+
+                    groupButtons.appendChild(deleteButton)
+                    groupButtons.appendChild(editButton)
+                    contactDiv.appendChild(groupButtons)                    
                     cardBody.appendChild(contactDiv)
                 }
             })
